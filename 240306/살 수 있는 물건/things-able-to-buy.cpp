a3b866df1a68956eec27ccbad18de5b5b24a8702
Joe <117707033+JoeYunHa@users.noreply.github.com>
2024-03-06 13:21:06 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240306/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	if (n - 3000 > 0)
+		cout << "book" << endl;
+	else if (n - 1000 > 0)
+		cout << "mask" << endl;
+	else
+		cout << "no" << endl;
+
+	return 0;
+}
