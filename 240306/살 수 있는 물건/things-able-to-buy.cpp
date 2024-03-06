07b77456e2034a7f29f4c0359af0208cc9d926c9
Joe <117707033+JoeYunHa@users.noreply.github.com>
2024-03-06 13:21:41 +0900

    Title: 살 수 있는 물건, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/살 수 있는 물건/things-able-to-buy.cpp
+++ 240306/살 수 있는 물건/things-able-to-buy.cpp
@@ -5,9 +5,9 @@ int main() {
 	int n;
 	cin >> n;
 
-	if (n - 3000 > 0)
+	if (n - 3000 >= 0)
 		cout << "book" << endl;
-	else if (n - 1000 > 0)
+	else if (n - 1000 >= 0)
 		cout << "mask" << endl;
 	else
 		cout << "no" << endl;
