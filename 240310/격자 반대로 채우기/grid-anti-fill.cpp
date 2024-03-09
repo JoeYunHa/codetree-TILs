83c75975c76ef19a3d649644c8ccbdb74b6fca9e
Joe <117707033+JoeYunHa@users.noreply.github.com>
2024-03-10 00:35:09 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240310/격자 반대로 채우기/grid-anti-fill.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<vector<int>> v(n, vector<int>(n, 0));
+
+	int ele{ 1 };
+
+	for (int col{ n - 1 }; col >= 0; col--) {
+		if (col % 2 != 0) {
+			for (int row{ n - 1 }; row >= 0; row--) {
+				v[row][col] = ele;
+				ele++;
+			}
+		}
+		else {
+			for (int row{ 0 }; row < n; row++) {
+				v[row][col] = ele;
+				ele++;
+			}
+		}
+	}
+
+	for (const auto& row : v) {
+		for (const auto& ele : row) {
+			cout << ele << " ";
+		}
+		cout << "\n";
+	}
+}
