5887fb33fe135a144e1085d1897be3caf7bc94a6
Joe <117707033+JoeYunHa@users.noreply.github.com>
2024-03-10 00:37:37 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/격자 반대로 채우기/grid-anti-fill.cpp
+++ 240310/격자 반대로 채우기/grid-anti-fill.cpp
@@ -9,18 +9,35 @@ int main() {
 	vector<vector<int>> v(n, vector<int>(n, 0));
 
 	int ele{ 1 };
-
-	for (int col{ n - 1 }; col >= 0; col--) {
-		if (col % 2 != 0) {
-			for (int row{ n - 1 }; row >= 0; row--) {
-				v[row][col] = ele;
-				ele++;
+	if (n % 2 == 0) {
+		for (int col{ n - 1 }; col >= 0; col--) {
+			if (col % 2 != 0) {
+				for (int row{ n - 1 }; row >= 0; row--) {
+					v[row][col] = ele;
+					ele++;
+				}
+			}
+			else {
+				for (int row{ 0 }; row < n; row++) {
+					v[row][col] = ele;
+					ele++;
+				}
 			}
 		}
-		else {
-			for (int row{ 0 }; row < n; row++) {
-				v[row][col] = ele;
-				ele++;
+	}
+	else {
+		for (int col{ n - 1 }; col >= 0; col--) {
+			if (col % 2 == 0) {
+				for (int row{ n - 1 }; row >= 0; row--) {
+					v[row][col] = ele;
+					ele++;
+				}
+			}
+			else {
+				for (int row{ 0 }; row < n; row++) {
+					v[row][col] = ele;
+					ele++;
+				}
 			}
 		}
 	}
