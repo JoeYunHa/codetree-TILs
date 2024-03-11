9b5666c93faa2be36ea1d6088942d969a7c003b0
Joe <117707033+JoeYunHa@users.noreply.github.com>
2024-03-12 04:48:12 +0900

    Title: 정수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/정수만 더하기/add-only-integers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    int len = str.length();
+    int sum{ 0 };
+
+    for (int i{ 0 }; i < len;i++) {
+        if (isdigit(str[i]) != 0)
+            sum += str[i] - '0';
+    }
+    cout << sum;
+}
